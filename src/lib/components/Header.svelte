396bde403c2abb124804bd99ab6a0b681efa8f58
Gleb Stepanov <gleb.stepanov@online.de>
2025-03-05 23:01:09 +0100

    Add Header component and integrate it into the main page

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex-col items-center justify-center">
+	<h1>Header</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1 class="font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+
+<p>Example Page content</p>
