2bbf5432e05c0595a086e5c0794312b380214d46
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-04 23:28:34 +0200

    removed flex from the Link

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -2,7 +2,7 @@
 	let { linkAttribute } = $props();
 </script>
 
-<div class="inline-flex h-4 items-center justify-center gap-3">
+<div class="h-4 items-center justify-center gap-3">
 	<div class="font-robotoflex relative justify-start text-center text-xs font-normal">
 		<a href={linkAttribute.href}>{linkAttribute.name}</a>
 	</div>
