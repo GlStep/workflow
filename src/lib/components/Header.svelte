0e7a010a00a5551f6d47e1936c48ba333d372cb7
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-06 22:31:24 +0200

    Refactor Header component to update import paths and enhance layout structure

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import LinkContainer from '$lib/components/LinkContainer.svelte';
+	import LinkContainer from '$lib/components/Containers/LinkContainer.svelte';
 	import Logo from '$lib/components/Logo.svelte';
+	import ButtonContainer from '$lib/components/Containers/SignInUpButtonContainer.svelte';
 
 	let headerLinks = [
 		{ name: 'Home', href: '/' },
@@ -9,7 +10,16 @@
 	];
 </script>
 
-<div class="relative inline-flex w-full items-center justify-between px-20">
-	<Logo />
-	<LinkContainer linkObjects={headerLinks} />
+<div
+	class="relative inline-flex h-30 w-full items-center justify-between self-stretch rounded-2xl px-20 py-2"
+>
+	<div
+		class="inline-flex h-full w-full max-w-6xl items-center justify-between rounded-2xl bg-zinc-200 px-24"
+	>
+		<div class="inline-flex items-center justify-center gap-20">
+			<Logo />
+			<LinkContainer linkObjects={headerLinks} />
+		</div>
+		<ButtonContainer />
+	</div>
 </div>
