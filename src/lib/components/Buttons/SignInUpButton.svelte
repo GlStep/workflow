ba19241077db4cb2c7957cd1ec5f8b472ad9afb1
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-06 22:31:10 +0200

    Add SignInUpButton and SignInUpButtonContainer components

---
+++ src/lib/components/Buttons/SignInUpButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let { signInUpAttribute, variant } = $props();
+	let textColor = $state();
+	let bg = $state();
+
+	if (variant === 'dark') {
+		bg = 'bg-black';
+		textColor = 'text-white';
+	} else if (variant === 'light') {
+		bg = 'bg-white';
+		textColor = 'text-black';
+	}
+</script>
+
+<a href={signInUpAttribute.href}>
+	<button
+		class="{bg} inline-flex items-center justify-center gap-2.5 rounded-lg px-6 py-2 outline-1 outline-black hover:cursor-pointer"
+	>
+		<div class="font-robotoflex text-center {textColor}">{signInUpAttribute.text}</div>
+	</button>
+</a>
