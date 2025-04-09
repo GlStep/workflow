322d80ab7925922984e8962f9fff3e080e8fda0e
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-09 23:17:30 +0200

    following Lucia Handbook up to CSRF

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
-import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
