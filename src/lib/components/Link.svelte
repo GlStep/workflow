f61a518894f65972d3c40acb43ebfcbc1097a27c
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-12 12:43:01 +0200

    First vue integration, moved files from Svelte to vue

--- src/lib/components/Link.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let { linkAttribute } = $props();
-</script>
-
-<div class="h-4 items-center justify-center gap-3">
-	<div class="font-robotoflex relative justify-start text-center text-xs font-normal">
-		<a href={linkAttribute.href}>{linkAttribute.name}</a>
-	</div>
-</div>
