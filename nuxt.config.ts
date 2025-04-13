95c9bf2b284073293b64124ad9ff44cbd34d7ca3
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-13 21:45:42 +0200

    Make TailwindCSS work nicely and how I want

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: {
-    enabled: false,
+    enabled: true,
   },
-
+  css: ["~/assets/css/main.css"],
   modules: [
     "@nuxt/eslint",
     "@nuxt/fonts",
     "@nuxt/icon",
     "@nuxt/test-utils",
     "@nuxt/scripts",
-    "@nuxtjs/tailwindcss",
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
