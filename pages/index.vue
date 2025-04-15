feeadea233fe1d34faa9ec86b3a5cf21d5d68494
Gleb Stepanov <gleb.stepanov@online.de>
2025-04-15 20:13:22 +0200

    Update Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <h1 class="font-poppins">
-      Testing
-    </h1>
-    <h2 class="font-roboto text-2xl">
-      Testing stuff
-    </h2>
+    <Header />
   </div>
 </template>
