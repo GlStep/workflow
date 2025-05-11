a229830e59ab095702aaf3e9c414e1b60cf05922
Gleb Stepanov <gleb.stepanov@online.de>
2025-05-11 11:59:20 +0200

    make list of todos for the application

--- pages/app.vue
+++ pages/app.vue
@@ -12,6 +12,12 @@ async function signOut() {
 }
 </script>
 
+<!-- List of stuff, which needs to be done for the application -->
+<!-- TODO: fetch, if user is signed in or not, redirect if needed -->
+<!-- TODO: create a sidebar, just a first and simple version (will be more complex later) -->
+<!-- TODO: create schema, which fits has user information, such as workspaces,groups, etc. -->
+<!-- TODO: extend the sidebar with the added information -->
+
 <template>
   <div>
     App
