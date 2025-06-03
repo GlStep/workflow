be04609197edb29d119caf9150e5a45d93aec4a3
Gleb Stepanov <gleb.stepanov@online.de>
2025-06-03 23:24:37 +0200

    Create NavUser component, for the user

--- components/nav/user.vue
+++ components/nav/user.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import { DropdownMenu, DropdownMenuTrigger } from '~/components/ui/dropdown-menu'
+import { useSidebar } from '~/components/ui/sidebar'
 
+const props = defineProps<{
+  user: {
+    name: string
+    email: string
+    avatar: string
+  }
+}>()
+
+const { isMobile } = useSidebar()
 </script>
 
 <template>
+  <SidebarMenu>
+    <SidebarMenuItem>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <SidebarMenuButton>
+            <Avatar>
+              <AvatarImage :src="user.avatar" :alt="user.name">
+                <AvatarFallback>IM</AvatarFallback>
+              </AvatarImage>
+            </Avatar>
+          </SidebarMenuButton>
+        </DropdownMenuTrigger>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  </SidebarMenu>
 </template>
 
 <style scoped>
